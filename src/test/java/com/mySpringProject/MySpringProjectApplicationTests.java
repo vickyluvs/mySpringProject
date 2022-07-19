12a12215fc3ad77b87d335b64bc8501aa9d9f048
Victoria.T <hturay1@gmail.com>
2022-07-19 13:43:17 -0400

    first commit

---
+++ src/test/java/com/mySpringProject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
